c9ae5057aa01c8efbb06417bca792d9156d0b136
Francisco Frutuoso <franciscofrut@gmail.com>
2017-11-23 19:37:22 +0000

    add points in path by duplicating layers

--- AE_control-Path-By-Named-Nulls.js
+++ AE_control-Path-By-Named-Nulls.js
@@ -1,7 +1,7 @@
 // Control path by named layers
 // apply to any path property
 
-var useName = false; // use path layer name
+var useName = true; // use path layer name
 var searchString = "PathPoint_"; // string to search
 
 // search by path layer name
@@ -16,13 +16,19 @@ var inTangArr = originalPath.inTangents();
 var outTangArr = originalPath.outTangents();
 
 // search layers
-for (var i = 1; i < thisComp.numLayers + 1; i++) {
+for (var i = thisComp.numLayers; i >= 1; i--) {
   var nameSearch = thisComp.layer(i).name.search(searchString);
 
   // if any Layer name matches searchString
   if (nameSearch >= 0) {
     var pointIndex = Number(thisComp.layer(i).name.substr(searchString.length)); // get point index from name
+    if (pointIndex >= pointArr.length) {
+      pointArr.push([0, 0]);
+      inTangArr.push([0, 0]);
+      outTangArr.push([0, 0]);
+    }
     pointArr[pointIndex] = fromCompToSurface(thisComp.layer(i).toComp(thisComp.layer(i).anchorPoint)); // set position
+
   }
 }
 
